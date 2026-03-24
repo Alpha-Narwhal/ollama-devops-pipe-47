f97026f65fca8f6a701d55c3dff2b21a543d9e0e
HyperMass02 <163587598+HyperMass02@users.noreply.github.com>
2026-03-24 13:34:39 -0600

    Added Terraform & AWS

---
+++ iac/main.tf
@@ -0,0 +1,17 @@
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              echo "Hello from Terraform" > /usr/share/nginx/html/index.html
+              EOF
+
+  tags = {
+    Name = "Ollama"
+  }
+}
